a8957dfbfc05e647a66db21fb982b6d2e615ddab
DEEPIKA-SUK <sukhijadeepika2001@gmail.com>
2021-04-14 07:42:56 +0530

    Improved blogs functionalities

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,10 +1,16 @@
-const Article = require('./../models/article')
+const Article = require("./../models/article");
 const blogController = () => {
   // factory functions
   return {
     blog: async (req, res) => {
-      const blogs = await Article.find().sort({ createdAt: 'desc' })
-      return res.render("blog", { user: req.user, blogs:blogs });
+      const blogs = await Article.find({ category: req.params.slug }).sort({
+        createdAt: "desc",
+      });
+      return res.render("blog", {
+        user: req.user,
+        blogs: blogs,
+        category: req.params.slug,
+      });
     },
   };
 };
